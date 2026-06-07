856085457180fad42cea47b3d7178d8e6fc14cbe
orlandoford36-lab <orlandoford36@gmail.com>
2026-06-07 15:11:26 -0500

    Remove unused useState import in HotCollections

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import AOS from "aos";
 import { Link } from "react-router-dom";
 import Slider from "react-slick";
